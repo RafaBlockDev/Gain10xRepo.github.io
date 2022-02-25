c3208f669b5a9f964f815cc6643792792ff7a2de
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-02-25 11:33:08 -0600

    Uploading a new user into the strapi admin panel

--- config/database.js
+++ config/database.js
@@ -1,4 +1,25 @@
+import axios from "axios";
+
 const path = require('path');
+// Request API.
+// Add your own code here to customize or restrict how the public can register new users.
+axios
+  .post('http://localhost:1337/api/auth/local/register', {
+    username: 'Strapi user',
+    email: 'user@strapi.io',
+    password: 'strapiPassword',
+  })
+  .then(response => {
+    // Handle success.
+    console.log('Well done!');
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
+
 
 module.exports = ({ env }) => ({
   connection: {
